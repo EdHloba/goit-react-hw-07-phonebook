b24dfb1739f83ccfa83015ef69631974dc3838a8
EdHloba <hloba.e.v@gmail.com>
2023-08-19 23:56:37 +0300

    hw-07-done

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-size: 22px;
+        color: var (--primary-text-color);
+
+        padding-left: 10px;
+`;
+
+export const Title = styled.h1`
+        text-transform: uppercase;
+        margin-top: 20px;
+        margin-bottom: 20px;
+`;
+
+export const SubTitle = styled.h2`
+        margin-top: 20px;
+        margin-bottom: 20px;
+`
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,63 @@
+import { FormStyled, Label, Input, Error, Button } from './ContactForm.styled';
+import { Formik, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { useDispatch, useSelector } from "react-redux";
+import { selectContacts } from 'redux/selectors';
+import { addContact } from 'redux/operations';
+import { Notify } from 'notiflix';
+
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .required('Name is required')
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      'Invalid name'
+    ),
+  number: Yup.string()
+    .length(9)
+    .required('Phone number is required')
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d/,
+      'Invalid number'
+    ),
+});
+
+const initialValues = {
+    name: '',
+    number: ''
+};
+
+export const ContactForm = () => {
+    const dispatch = useDispatch();
+    const contacts = useSelector(selectContacts);
+
+    const handleSubmit = (values, { resetForm }) => {
+        const newName = contacts.some(contact =>
+                contact.name.toLowerCase() === values.name.toLowerCase());
+                if (newName) {
+                    Notify.failure(`${values.name} is already in contacts`);
+                    return;
+                }
+        dispatch(addContact({ name: values.name, number: values.number }));
+        resetForm();
+        Notify.success(`${values.name} is added to contacts`);
+    };
+
+        return (
+             <Formik 
+                initialValues={initialValues} 
+                onSubmit={handleSubmit}
+                validationSchema={schema}>
+                <FormStyled autoComplete="off" >
+                    <Label htmlFor='name'>Name</Label>
+                    <Input type="text" name="name" placeholder="Jacob Mercer" />
+                    <ErrorMessage component={Error} name="name" />
+
+                    <Label htmlFor='number'>Number</Label>
+                    <Input type="tel" name="number" placeholder="787-78-78" maxLength="9" />
+                    <ErrorMessage component={Error} name="number"/>
+                    <Button type="submit">Add contact</Button>
+                </FormStyled>
+            </Formik>
+        );
+};
